93d69e327e1fc8d6b8014e092938bddc18619729
Nattapol0164 <68050164@kmitl.ac.th>
2026-09-21 14:50:33 +0700

    Create play_with_arrays.py

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,11 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+i = 0
+while i < len(original_array):
+    if original_array[i] > 5:
+        new_array.append(original_array[i] + 2)
+    i += 1
+
+print(original_array)
+print(new_array)
